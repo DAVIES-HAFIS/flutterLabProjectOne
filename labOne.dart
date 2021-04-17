d145269253f7135d38fc1e3bd4b236e239e142b2
DAVIES-HAFIS <tejnik2007@yahoo.co.uk>
2021-04-17 23:17:37 +0100

    flutter individual project

---
+++ labOne.dart
@@ -0,0 +1,27 @@
+void main() {
+  int n = 17;
+  List y = [];
+  for (int i = 1; i <= n; i++) {
+    if (n % i == 0) {
+      y.add(i);
+    }
+  }
+  if (y.length <= 2) {
+    print('$n is a prime number');
+  } else {
+    print('$n is a constutution number and is not a prime number');
+  }
+
+  int b = 57;
+  List v = [];
+  for (int i = 1; i <= b; i++) {
+    if (b % i == 0) {
+      v.add(i);
+    }
+  }
+  if (v.length <= 2) {
+    print('$b is a prime number');
+  } else {
+    print('$b is a constutution number and is not a prime number');
+  }
+}
